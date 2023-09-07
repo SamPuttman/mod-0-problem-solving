5435be25df70c6c996102794362db4318aa95a4d
Sam Puttman <daputter2@gmail.com>
2023-09-07 17:38:30 -0500

    add another answer

--- challenge.rb
+++ challenge.rb
@@ -81,7 +81,10 @@ end
 # Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized. 
 # For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
 
-#
+sentence = "Turing is the best"
+
+sentence.split(' ').each(&:capitalize).join(' ')
+
 
 
 # Write a method or function that determines how much a person will [pay in taxes in the United States](https://www.irs.gov/newsroom/irs-provides-tax-inflation-adjustments-for-tax-year-2022) based on their annual income. The method or function should accept a number representing the individual's annual income as an argument and return the amount they will owe in taxes for that year.
